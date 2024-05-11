476edad31c1c1955fe0dd902b8bd8285425c470c
DESKTOP-EPEBRN7\charlotte <things8134@naver.com>
2024-05-12 00:48:19 +0900

    cors2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: '*',
     method: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
     optionsSuccessStatus: 200
